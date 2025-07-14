92b15b2edd1f1f96d9f022759e95a01ff4ca2813
armin1864 <arminsuleimani@gmail.com>
2025-07-14 22:50:29 +0330

    fix : change in structure of process instructions

--- src/Process.java
+++ src/Process.java
@@ -1,28 +1,46 @@
 public class Process {
-    private int ic;
-    private String[] instructions;
+
+    class instruction{
+        String name;
+        int T;
+        int X;
+        int Y;
+        int A;
+        instruction(String s){
+            name = s;
+            T = X = Y = A = -1;
+        }
+    }
+
+    int ic;
+    instruction[] instructions;
     private int i=0;
     public Process(int n){
         ic = n;
-        instructions = new String[ic];
+        instructions = new instruction[ic];
     }
 
-    public int getIc(){
-        return ic;
-    }
-
-    public String[] getInstructions(){
-        return instructions;
-    }
-
-    public void printInstructions(){
-        for(String s : instructions) System.out.println(s);
-    }
 
     public void addInstruction(String instruction){
         if(i<instructions.length) {
-            instructions[i] = instruction;
+            String name = instruction.split("\\s+")[0];
+            instruction ins = new instruction(name);
+            switch (name) {
+                case "Run", "Sleep" -> ins.T = Integer.parseInt(instruction.split("\\s+")[1]);
+                case "Allocate", "Free" -> {
+                    ins.X = Integer.parseInt(instruction.split("\\s+")[1]);
+                    ins.Y = Integer.parseInt(instruction.split("\\s+")[2]);
+                }
+                case "Read", "Write" -> ins.A = Integer.parseInt(instruction.split("\\s+")[1]);
+            }
+            instructions[i] = ins;
             i++;
         } else System.out.println("ERROR: instructions numbers are finished");
+    }
+
+    public void printInstructions(){
+        for(instruction i : instructions){
+            System.out.println(i.name + i.T + i.X + i.Y + i.A);
+        }
     }
 }
